715eb36a01411ebf2a0d18d7750d63e0ea13937b
Victor Quiñones <vd-victor@hotmail.com>
2016-10-18 00:54:22 -0500

    App
    Reestructuracion del proyecto app, ahora se trabaja con gulp.
    Login funcionando correctamente.
    
    Proximo paso: implementar el menú, corregir la vista del home

---
+++ app/src/app.js
@@ -0,0 +1,64 @@
+/**
+ * Created by user on 19/04/2016.
+ */
+
+(function () {
+    angular
+        .module('app', ['ngRoute', 'satellizer', 'ngStorage'])
+
+        .constant('config', {
+            APIURL: "http://api.notagile.com"
+        })
+        .config(["$routeProvider", "config", "$authProvider",
+            function ($routeProvider, config, $authProvider) {
+
+                //Configuraciones referentes al token
+                $authProvider.httpInterceptor = function () {
+                    return true;
+                };
+                $authProvider.loginUrl = config.APIURL + '/login';
+                $authProvider.tokenName = 'token';
+                $authProvider.tokenHeader = 'Authorization';
+                $authProvider.tokenType = 'Bearer';
+                $authProvider.storageType = 'localStorage';
+
+                $routeProvider.when('/', {
+                        redirectTo: "/home"
+                    })
+                    .when("/home", {
+                        templateUrl: 'components/home/home.html',
+                        controller: 'homeController',
+                        authorization: true
+                    })
+                    .when("/login", {
+                        templateUrl: 'components/login/login.html',
+                        controller: 'loginController',
+                        authorization: false,
+                        controllerAs: 'ctrl'
+                    })
+            }])
+
+        .run(["$rootScope", '$location', '$auth',
+            function ($rootScope, $location, $auth) {
+                $rootScope.$on('$routeChangeStart', function (event, next) {
+
+                    var token = $auth.getToken() || false;
+
+                    /*Si la URL a la que se va a acceder es el Login entonces se agrega una clase al body necesaria para
+                     la vista del login*/
+                    if (next.originalPath == '/login') {
+                        $rootScope.bodyLayout = "login-layout blur-login";
+                    } else {
+                        $rootScope.bodyLayout = "";
+                    }
+
+                    //La siguiente condicion controla que el usuario este autenticado, si no lo esta redirecciona al login.
+                    if (!token || !$auth.isAuthenticated()) {
+                        $location.path("/login");
+                        return true;
+                    }
+
+                });
+
+            }]);
+})(angular);
