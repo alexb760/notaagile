f503bc202b93fbaf9a19f396be8bc845c27bd05d
Victor Quiñones <vd-victor@hotmail.com>
2016-10-22 16:34:38 -0500

    Se versiona la migración para la tabla alumnos

---
+++ api/database/migrations/2016_09_28_184335_create_alumnos_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlumnosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumnos', function (Blueprint $table) {
+            $table->increments("id");
+            $table->string("nombres", 200);
+            $table->string("apellidos", 200);
+            $table->string("doc_identificacion", 200);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop("alumnos");
+    }
+}
